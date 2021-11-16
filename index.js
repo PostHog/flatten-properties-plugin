ad95a2010240568f47a20590813c309fe51e4b6f
Tim Glaser <tim@posthog.com>
2021-11-16 16:10:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const flattenProperties = (props, sep, nestedChain = []) => {
         if (key === '$elements') {
             // Don't expand $elements on $autocapture events as those will be removed anyway
         } else if (key === '$set') {
-            newProps = {...newProps, $set: flattenProperties(props['$set'], sep)}
+            newProps = {...newProps, $set: {...props[key], ...flattenProperties(props[key], sep)}}
         } else if (key === '$set_once) {
-            newProps = {...newProps, $set_once: flattenProperties(props['$set'], sep)}
+            newProps = {...newProps, $set_once: {...props[key], flattenProperties(props[key], sep)}}
         } else if (Array.isArray(value)) {
             let objectFromArray = {}
             for (let i = 0; i < value.length; ++i) {
