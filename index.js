0900d76a76b7d9620f5f2b5f0ba95865f1987200
Neil Kakkar <neilkakkar@gmail.com>
2022-12-19 15:43:49 +0000

    feat(group-analytics): Add support for flattenning group properties

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const flattenProperties = (props, sep, nestedChain = []) => {
             newProps = { ...newProps, $set: { ...props[key], ...flattenProperties(props[key], sep) } }
         } else if (key === '$set_once') {
             newProps = { ...newProps, $set_once: { ...props[key], ...flattenProperties(props[key], sep) } }
+        } else if (key === '$group_set') {
+            newProps = { ...newProps, $group_set: { ...props[key], ...flattenProperties(props[key], sep) } }
         } else if (Array.isArray(value)) {
             newProps = { ...newProps, ...flattenProperties(props[key], sep, [...nestedChain, key]) }
         } else if (value !== null && typeof value === 'object' && Object.keys(value).length > 0) {
